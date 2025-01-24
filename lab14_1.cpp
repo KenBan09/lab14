0b37c21d128b8c07a43ff13e7f2d0b338d6de60b
KenBan09 <keeno.pkt@gmail.com>
2025-01-24 15:18:38 +0700

    lab14_1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	for( int k = 1; k < N ; k++){
+		for( int j = k; j>0 ; j--){
+		if(d[j-1]<= d[j] ){
+			T temp = d[j];
+			d[j] = d[j-1];  
+			d[j-1]= temp;
+		}
+			
+		
 
+		} cout << "Pass " << k << ":";
+		for( int n = 0 ; n < N ; n++){
+			cout << d[n] << " ";
+		}cout << endl;
+
+	}
 }
 
 int main(){
@@ -17,3 +32,4 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+ 
